f784511538f26186ea165c8b6378861ea825ceed
alep-2000 <alessiopiras056@gmail.com>
2023-06-01 22:03:20 +0200

    [LOGIC]Snack 1 completed

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
 "use strict"
+
+// ARRAY INVITATI
+const invited = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+console.log(invited);
+
+let arrayObje = invited.map((item,index) =>{
+    let obj = {
+        'table_name': 'Tavolo Vip',
+        'name': item,
+        'place_occuped': index + 1
+    }
+    return obj
+})
+
+console.log(arrayObje);
