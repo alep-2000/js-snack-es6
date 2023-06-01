63657ca8fe6e639df0df43fbe4a41814c98c5ea6
alep-2000 <alessiopiras056@gmail.com>
2023-06-01 23:13:39 +0200

    [LOGIC]Snack 2 completed

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 "use strict"
 
-// ARRAY INVITATI
+// SNACK 1
+// ARRAY INVITED
 const invited = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
-console.log(invited);
 
+// NEW ARRAY
 let arrayObje = invited.map((item,index) =>{
+    // OBJECT
     let obj = {
         'table_name': 'Tavolo Vip',
         'name': item,
@@ -14,4 +16,71 @@ let arrayObje = invited.map((item,index) =>{
     return obj
 })
 
-console.log(arrayObje);
+// SNACK 2
+// ARRAY STUDENTS
+const students = [
+    {
+        'id': '213',
+        'name': 'Marco della Rovere',
+        'grades': '78'
+    },
+    {
+        'id': '110',
+        'name': 'Paola Cortellessa',
+        'grades': '96'
+    },
+    {
+        'id': '250',
+        'name': 'Andrea Mantegna',
+        'grades': '48'
+    },
+    {
+        'id': '145',
+        'name': 'Gaia Borromini',
+        'grades': '74'
+    },
+    {
+        'id': '196',
+        'name': 'Luigi Grimaldello',
+        'grades': '68'
+    },
+    {
+        'id': '102',
+        'name': 'Piero della Francesca',
+        'grades': '50'
+    },
+    {
+        'id': '120',
+        'name': 'Francesca da Polenta',
+        'grades': '84'
+    },
+]
+
+// UPPERCASE ARRAY
+let uppercase_array = students.map((item) => {
+    let name = item.toLowerCase();
+    let newName = name[0].toUpperCase() + name.slice(1);
+    
+    return newName
+});
+console.log(uppercase_array);
+
+// ARRAY GRADES_PLUS
+let grades_plus = students.filter((item) => {
+    if(item.grades > 70){
+        return true;
+    }
+    return false;
+})
+
+console.log(grades_plus);
+
+// ARRAY IDGRADES_PLUS
+let idGradesPlus = students.filter((item) => {
+    if(item.grades > 70 && item.id > 120){
+        return true;
+    }
+    return false;
+})
+
+console.log(idGradesPlus);
